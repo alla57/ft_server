c9bf05fdc3f71a75652dabc2a023d77c5cb83e2d
Alla eddine BOUMEDIENE <alboumed@e3r5p18.42.fr>
2021-03-07 17:10:33 +0100

    push

--- commands.sh
+++ commands.sh
@@ -5,19 +5,27 @@ mysql_secure_installation
 
 apt update
 apt install -y nginx
-service nginx start
 
-apt update
+apt install -y wget php (php-cgi php-mysqli php-pear) php-mbstring (php-gettext php-common php-phpseclib) php-mysql
+or
+apt install apache2 php (php-json) php-mbstring (php-zip php-gd php-xml php-curl) php-mysql
+or
 apt install php-mbstring php-zip php-gd
 
-apt-get update
-apt-get install phpmyadmin
-
-apt install wget -y
+apt install -y wget
 wget -P Downloads https://www.phpmyadmin.net/downloads/phpMyAdmin-latest-all-languages.tar.gz
+tar xvf Downloads/phpMyAdmin-latest-all-languages.tar.gz
+mv phpMyAdmin-*/ /usr/share/phpmyadmin
 
-apt-get install wget
+service mysql start
+service nginx start
+service php7.3-fpm start
+
+CREATE USER 'nom_utilisateur_choisi'@'localhost' IDENTIFIED BY 'mot_de_passe_solide';
+GRANT ALL ON *.* TO 'nom_utilisateur_choisi'@'localhost' WITH GRANT OPTION;
+FLUSH PRIVILEGES;
+QUIT;
+
 apt install gnupg
 apt-get update && apt-get install lsb-release
-apt update
-apt install mysql-server
+apt install mysql-server mysql-client
