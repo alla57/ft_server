023a494a345ff76046d89f1c9e00a1dc3ef3faa9
user42 <a.boumediene01@gmail.com>
2021-03-13 20:19:28 +0100

    dlkfslkjdf

--- commands.sh
+++ commands.sh
@@ -6,7 +6,7 @@ mysql_secure_installation
 apt update
 apt install -y nginx
 
-apt-get install openssl
+apt-get install -y openssl
 
 apt-get install -y php-fpm php-mysqli php-xml php-mbstring(le mien)
 (peut etre aussi) php-curl
@@ -29,19 +29,23 @@ service mysql start
 mysql -u root
 ALTER USER 'root'@'localhost' IDENTIFIED BY '1234';
 
-openssl req -config example-com.conf -new -x509 -sha256 -newkey rsa:2048 -nodes -keyout key.key -days 365 -out cert.cert
+openssl req -x509 -newkey rsa:4096 -keyout key.key -out cert.cert -days 365
+#openssl req -config example-com.conf -new -x509 -sha256 -newkey rsa:2048 -nodes -keyout key.key -days 365 -out cert.cert
 
 vim /etc/nginx/sites-available/default
 
-ssl_certificate /etc/ssl/ssl-bundle.crt;
-ssl_certificate_key /etc/ssl/ssl-tutorials.key;
+ssl_certificate /cert.cert;
+ssl_certificate_key /key.key;
+
+# ssl_certificate /etc/ssl/ssl-bundle.cert;
+# ssl_certificate_key /etc/ssl/ssl-tutorials.key;
 
 service mysql restart
 service nginx start
 service php7.3-fpm start
 
 wget -P Downloads https://wordpress.org/latest.tar.gz
-tar xvf Downloads/latest.tag.gz
+tar xvf Downloads/latest.tar.gz
 mv /wordpress /var/www/html
 chown -R www-data:www-data /var/www/html/wordpress/
 chmod 755 -R /var/www/html/wordpress/
@@ -50,6 +54,8 @@ CREATE DATABASE wordpress;
 GRANT ALL PRIVILEGES on wordpress.* TO 'root'@'localhost' IDENTIFIED BY '1234';
 FLUSH PRIVILEGES;
 EXIT;
+
+mysql -u username -p database_name < file.sql
 
 # apt install -y wget php (php-cgi php-mysqli php-pear) php-mbstring (php-gettext php-common php-phpseclib) php-mysql
 # or
---
+++ wordpress_db.sql
@@ -0,0 +1,4 @@
+CREATE DATABASE wordpress;
+GRANT ALL PRIVILEGES on wordpress.* TO 'root'@'localhost' IDENTIFIED BY '1234';
+FLUSH PRIVILEGES;
+EXIT;
